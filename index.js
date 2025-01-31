e993f83cafe0abd011c60996a05b287582dfff0a
FranklinMburuDev <184284364+FranklinMburu@users.noreply.github.com>
2025-01-31 03:43:07 +0300

    reduce

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+
+
+// Use reduce() to sum the values of batteryBatches
+const totalBatteries = batteryBatches.reduce((accum, current) => accum + current, 0);
+
+// Export the result for testing
+module.exports = {
+  totalBatteries
+};
